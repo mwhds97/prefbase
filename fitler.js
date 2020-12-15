effd86dafc8ccc8eec4add1dc3b67439e8498089
Optimus Primal <32876945+mwhds97@users.noreply.github.com>
2020-12-15 19:43:05 +0800

    Add files via upload

--- fitler.js
+++ fitler.js
@@ -1,15 +1,10 @@
 function filter(node) {
-	const info = JSON.parse(node.ProxyInfo);
-	if(info.Remark.toLowerCase().includes("iepl") || info.Remark.toLowerCase().includes("iplc") || info.Remark.toLowerCase().includes("bgp") || info.Remark.toLowerCase().includes("aga") || info.Remark.toLowerCase().includes("game")) {
-		if(info.Type.toLowerCase() == "ss") {
-			return true;
-		}
-		return false;
-	}
-	else {
-		if(info.Type.toLowerCase() == "trojan") {
-			return true;
-		}
-		return false;
-	}
+  const remark = JSON.parse(node.ProxyInfo).Remark.toLowerCase();
+  const proto = JSON.parse(node.ProxyInfo).Type.toLowerCase();
+  if(remark.includes("cen") || remark.includes("ctm") || remark.includes("iepl") || remark.includes("bgp") || remark.includes("aga") || remark.includes("game")) {
+    return proto == "ss";
+  }
+  else {
+    return proto == "trojan";
+  }
 }
--- sort.js
+++ sort.js
@@ -1,20 +1,20 @@
 function compare(node_a, node_b) {
-	var level_a = 0;
-	var level_b = 0;
-	const remark_a = JSON.parse(node_a.ProxyInfo).Remark.toLowerCase();
-	const remark_b = JSON.parse(node_b.ProxyInfo).Remark.toLowerCase();
-	var levels = {"cen+":12, "cen-":6, "cen":11, "ctm+":10, "ctm-":5, "ctm":9, "iepl+":8, "iepl-":4, "iepl":7, "bgp":3, "aga":2, "gia":1};
-	for(level in levels) {
-		if(remark_a.includes(level)) {
-			level_a = levels[level];
-			break;
-		}
-	}
-	for(level in levels) {
-		if(remark_b.includes(level)) {
-			level_b = levels[level];
-			break;
-		}
-	}
-	return level_a > level_b;
+  var level_a = 0;
+  var level_b = 0;
+  const remark_a = JSON.parse(node_a.ProxyInfo).Remark.toLowerCase();
+  const remark_b = JSON.parse(node_b.ProxyInfo).Remark.toLowerCase();
+  var levels = {"cen+":10, "cen-":5, "cen":9, "ctm":8, "iepl+":7, "iepl-":4, "iepl":6, "bgp":3, "aga":2, "gia":1};
+  for(level in levels) {
+    if(remark_a.includes(level)) {
+      level_a = levels[level];
+      break;
+    }
+  }
+  for(level in levels) {
+    if(remark_b.includes(level)) {
+      level_b = levels[level];
+      break;
+    }
+  }
+  return level_a > level_b;
 }
