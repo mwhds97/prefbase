f79cb22554a68313ea7f0f3aaa13fdb5139b2cf8
Optimus Primal <32876945+mwhds97@users.noreply.github.com>
2021-03-27 14:24:32 +0800

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,8 +1,8 @@
 function compare(node_a, node_b) {
   var level_a = 0;
   var level_b = 0;
-  const remark_a = JSON.parse(node_a.ProxyInfo).Remark.toLowerCase();
-  const remark_b = JSON.parse(node_b.ProxyInfo).Remark.toLowerCase();
+  const remark_a = node_a.Remark.toLowerCase();
+  const remark_b = node_b.Remark.toLowerCase();
   var levels = {" cc": 6, " iepl": 5, " ctm": 4, " bgp": 3, " aga": 2, " gia": 1};
   for(level in levels) {
     if(remark_a.includes(level)) {
@@ -18,4 +18,3 @@ function compare(node_a, node_b) {
   }
   return level_a > level_b;
 }
-
