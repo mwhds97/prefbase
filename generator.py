543e29c82321a19551c08728eb67c9b60ae7f285
mwhds97 <cjctcj68@gmail.com>
2021-06-29 08:32:44 +0800

    Update generator

--- generator.py
+++ generator.py
@@ -2,16 +2,14 @@
 # coding=utf-8
 
 import re
-import sys
 
 import yaml
 
 
 def level(node):
     levels = {
-        " CC": 7,
-        " AIA(?=\+)": 6,
-        " AIA(?!\+)": 5,
+        " CC": 6,
+        " AIA": 5,
         " IEPL": 4,
         " BGP": 3,
         " AGA": 2,
@@ -24,32 +22,47 @@ def level(node):
     return 0
 
 
-if "clash" in sys.argv[1]:
-    with open(sys.argv[1], "r", encoding="utf-8") as f:
-        conf = yaml.load(f, yaml.FullLoader)
-    conf["proxies"].sort(key=level, reverse=True)
-    with open("clash_sorted.list", "w", encoding="utf-8", newline="\n") as f:
-        yaml.dump(
-            conf,
-            f,
-            default_flow_style=False,
-            allow_unicode=True,
-            line_break="\n",
-            encoding="utf-8",
-            sort_keys=False,
-        )
-    with open("clash_remarks", "w", encoding="utf-8", newline="\n") as f:
-        for p in conf["proxies"]:
-            f.write("  - " + p["name"] + "\n")
+with open("clash.list", "r", encoding="utf-8") as f:
+    clash_list = yaml.load(f, yaml.FullLoader)
+clash_list["proxies"].sort(key=level, reverse=True)
+clash_nodes = (
+    yaml.dump(
+        clash_list,
+        default_flow_style=False,
+        allow_unicode=True,
+        line_break="\n",
+        encoding="utf-8",
+        sort_keys=False,
+    )
+    .decode("utf-8")
+    .strip()
+)
+clash_remarks = ""
+for p in clash_list["proxies"]:
+    clash_remarks += "  - " + p["name"] + "\n"
+clash_remarks = clash_remarks.strip()
+with open("Dler.yaml", "r+", encoding="utf-8", newline="\n") as f:
+    origin = f.read()
+    f.seek(0)
+    f.truncate()
+    f.write(
+        origin.replace("__nodes__", clash_nodes).replace("__remarks__", clash_remarks)
+    )
 
-if "surge" in sys.argv[1]:
-    with open(sys.argv[1], "r", encoding="utf-8") as f:
-        lines = f.readlines()
-    lines.sort(key=level, reverse=True)
-    with open("surge_sorted.list", "w", encoding="utf-8", newline="\n") as f:
-        for l in lines:
-            f.write(l)
-    with open("surge_remarks", "w", encoding="utf-8", newline="\n") as f:
-        for l in lines:
-            f.write("," + re.match(r"^(.*?) =", l).group(1))
-        f.write("\n")
+with open("surge.list", "r", encoding="utf-8") as f:
+    surge_list = f.readlines()
+surge_list.sort(key=level, reverse=True)
+surge_nodes = ""
+for l in surge_list:
+    surge_nodes += l
+surge_nodes = surge_nodes.strip()
+surge_remarks = ""
+for l in surge_list:
+    surge_remarks += "," + re.match(r"^(.*?) =", l).group(1)
+with open("Dler.conf", "r+", encoding="utf-8", newline="\n") as f:
+    origin = f.read()
+    f.seek(0)
+    f.truncate()
+    f.write(
+        origin.replace("__nodes__", surge_nodes).replace("__remarks__", surge_remarks)
+    )
