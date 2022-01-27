4bffa8a7ddd25c4a01e5db4660bedfa3dd9168d9
mwhds97 <cjctcj68@gmail.com>
2022-01-27 12:23:12 +0800

    Dler

--- jezoe.js
+++
@@ -1,8 +0,0 @@
-ssid = $network.wifi.ssid;
-if(!(ssid === null) && ssid.toLowerCase().includes("jezoe")) {
-  $surge.setSelectGroupPolicy("DomesticTV", "Proxy");
-}
-else {
-  $surge.setSelectGroupPolicy("DomesticTV", "DIRECT");
-}
-$done();
